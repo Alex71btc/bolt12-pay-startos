a675051d6153e40d458fde330461094cf01af954
Alexander Klemmstein <hardcorebtc@tutanota.com>
2026-05-22 23:33:12 +0200

    Bump package revision to 0.2.85:2

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_0_2_85_1 } from './v0.2.85.1'
+import { v_0_2_85_2 } from './v0.2.85.2'
 
 export const versionGraph = VersionGraph.of({
-  current: v_0_2_85_1,
+  current: v_0_2_85_2,
   other: [],
 })
---
+++ startos/versions/v0.2.85.2.ts
@@ -0,0 +1,13 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_0_2_85_2 = VersionInfo.of({
+  version: '0.2.85:2',
+  releaseNotes: {
+    en_US:
+      'Update bundled lndk-pay to latest upstream: clearer onion-message LND setup guidance in the web UI.',
+  },
+  migrations: {
+    up: async () => {},
+    down: IMPOSSIBLE,
+  },
+})
